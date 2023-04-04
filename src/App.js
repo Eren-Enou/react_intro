9483527d02a143ca3f2b3f0499b1162712ec392e
Eren-Enou <erenenou@yahoo.com>
2023-04-04 13:25:37 -0700

    added racer standings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import ButtonDisplay from './views/ButtonDisplay';
+import RacerDisplay from './views/RacerDisplay';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let name = 'Eren';
+
+    return (
+        <div className="App">
+            <Navbar username={name} city={"Cotati"}/>
+            <div className='container'>
+                <Routes>
+                    <Route path='/' element={<ButtonDisplay name={name} />} />
+                    <Route path='/racers' element={<RacerDisplay />} />
+                </Routes>
+                
+            </div>
+        </div>
+    );
 }
 
 export default App;
